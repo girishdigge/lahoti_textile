c747632571028770fa028a99059f841f55115bff
girishdigge <girishdigge@gmail.com>
2024-02-26 18:35:46 +0530

    dobby solution 2

--- src/app/(sitemap)/products/dobbyBorder/page.tsx
+++ src/app/(sitemap)/products/dobbyBorder/page.tsx
@@ -4,11 +4,11 @@ const Page = () => {
   return (
     <div className='flex flex-col flex-center justify-center'>
       <h1 className='mt-2 mb-2 z-10 text-5xl font-bold text-num-black flex justify-center bg-gradient-to-r from-num2-grad1 to-num2-grad2'>
-        DOBBY BORDER
+        Dobby Border
       </h1>
       <div className='grid grid-cols-2 gap-4'>
         <Image
-          className='h-auto max-w-full rounded-lg transform hover:scale-110 transition-all ease-in-out'
+          className='md:h-96 h-40 max-w-full rounded-lg transform hover:scale-110 transition-all ease-in-out'
           src='/products/dobby/1.jpg'
           height={500}
           width={500}
